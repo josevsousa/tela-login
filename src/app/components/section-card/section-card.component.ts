29d7195e1178ece5ded48e2eafe621d0d1b9d86e
josevsousa <jose.vicente.de.sousa@gmail.com>
2024-06-18 07:00:22 -0300

    separando o section card para um component

---
+++ src/app/components/section-card/section-card.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'section-card',
+    standalone: true,
+    template: `
+       <section class="card">
+            <div class="card-image" style="background-image: url('../../../assets/imagens/card-image-1.png');"></div>
+            <div class="card-main">
+                <img src="../../../assets/logo.svg">
+                <h1>Dev JoseVsousa</h1>
+                <p>Lorem ipsum dolor sit amet consectetur. Tortor varius suspendisse arcu amet scelerisque tincidunt neque
+                    posuere. Vehicula lectus eget</p>
+            </div>
+
+            <div class="card-footer">
+                <p>------- # ---------</p>
+                <h2>Dev: josevicentedesousa.surge.sh</h2>
+            </div>
+
+        </section>
+    `,
+    styleUrl: "./section-card.component.scss"
+})
+
+export class SectioncardComponent { }
--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,11 +1,15 @@
 import { BoundElementProperty } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { BottomPrimaryComponent } from '../../components/primary-bottom/primary-bottom.component';
+import { SectioncardComponent } from '../../components/section-card/section-card.component';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [BottomPrimaryComponent],
+  imports: [
+    BottomPrimaryComponent,
+    SectioncardComponent
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
